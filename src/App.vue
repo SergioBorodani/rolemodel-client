6eb5b733b9454c6f3595e742863c6a303cbaf839
s.borodanev <boss@curs.ru>
2020-03-16 15:25:35 +0300

    Second Commit

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,6 @@
 <template>
     <div id="app">
   <!--<v-app>-->
-    <!--<v-app-bar-->
-      <!--app-->
-      <!--color="primary"-->
-      <!--dark-->
-    <!--&gt;-->
-      <!--<div class="d-flex align-center">-->
-        <!--<v-img-->
-          <!--alt="Vuetify Logo"-->
-          <!--class="shrink mr-2"-->
-          <!--contain-->
-          <!--src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"-->
-          <!--transition="scale-transition"-->
-          <!--width="40"-->
-        <!--/>-->
-
-        <!--<v-img-->
-          <!--alt="Vuetify Name"-->
-          <!--class="shrink mt-1 hidden-sm-and-down"-->
-          <!--contain-->
-          <!--min-width="100"-->
-          <!--src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"-->
-          <!--width="100"-->
-        <!--/>-->
-      <!--</div>-->
-
-      <!--<v-spacer />-->
-
-      <!--<v-btn-->
-        <!--href="https://github.com/vuetifyjs/vuetify/releases/latest"-->
-        <!--target="_blank"-->
-        <!--text-->
-      <!--&gt;-->
-        <!--<span class="mr-2">Latest Release</span>-->
-        <!--<v-icon>mdi-open-in-new</v-icon>-->
-      <!--</v-btn>-->
-    <!--</v-app-bar>-->
 
     <!--<v-content>-->
 
@@ -58,17 +22,14 @@
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue';
-import PoljzovateljGridCard from './components/PoljzovateljGridCard'
-import RoljGridCard from './components/RoljGridCard'
-import RazrGridCard from './components/RazrGridCard'
+//import PoljzovateljGridCard from './components/PoljzovateljGridCard'
+//import RoljGridCard from './components/RoljGridCard'
+//import RazrGridCard from './components/RazrGridCard'
 
 export default {
   name: 'App',
 
   components: {
-      PoljzovateljGridCard,
-      RoljGridCard,
-      RazrGridCard
   },
 
   data: () => ({
